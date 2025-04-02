020746b37e17625decb0090dab80864e06650b9a
ClebersonCosta <cleberson-costa@outlook.com>
2025-04-02 18:04:35 -0300

    init: first commit

---
+++ src/test/java/com/algaworks/OrderTest.java
@@ -0,0 +1,12 @@
+package com.algaworks;
+
+import org.junit.jupiter.api.Test;
+
+public class OrderTest {
+
+    @Test
+    void shouldCreateOrder() {
+        Order order = new Order();
+    }
+
+}
